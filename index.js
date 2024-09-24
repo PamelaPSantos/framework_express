49b833f4828a20286f94b4f8e8378da09849e55f
Pamela Pereira dos Santos <pamela.santos@samisaude.com>
2024-09-24 15:45:16 -0300

    feat: tipos de resposta

--- index.js
+++ index.js
@@ -2,7 +2,18 @@ const express = require('express');
 const app = express();
 
 app.use('/', (req, res) => {
-    res.send("Hello World!");
+    // res.send("Hello World!");
+        res.json({
+            data: [
+                {id: 1, name: "João"},
+                {id: 2, name: "Maria"},
+                {id: 3, name: "Pedro"},
+            ],
+            count: 30,
+            skip: 0,
+            limit: 3,
+            status: 200
+        })
 })
 
 app.listen(8080, () => {
